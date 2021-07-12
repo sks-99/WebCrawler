c976c1477ce4e64f517b9f555f5b551ff8f266fd
Nicholas Chu <chu.Nicholas@hotmail.com>
2021-07-11 22:08:10 -0400

    add multithreading - run a thread for each search term

--- README.md
+++ README.md
@@ -7,9 +7,7 @@ Install beautifulsoup by following the instructions here https://www.crummy.com/
 Note: this was only tested on Windows
 Dataset folder must be created in the same directory as crawler.py and empty. since git cannot push an empty directory, you must create one until we have our own dataset
 1. put your search terms in the search_terms list
-2. in main(), choose whether you would like All Google Results or Google News results and run their respective functions
+2. in main(), choose whether you would like All Google Results or Google News results by passing their respective function into parse_webpage
 
 ## TO-DOs:
-- see if program is viable for use
-- this criteria needs to be met (has not been implemented)
 - come up with our own search terms (i.e., replace placeholders and generate the actual articles for the dataset)
--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 import urllib.request
 import os
 import time
+import threading
 from bs4 import BeautifulSoup
 
 # number of web pages saved
@@ -11,6 +12,7 @@ WORDS_MINIMUM = 200
 search_terms = ["facebook", "tiktok", "twitter"]
 page_delimiter = "&start={}"
 
+# user agent string
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
 
 # check word count on articles by counting words surrounded by paragraph tags
@@ -19,7 +21,7 @@ def word_count(url):
 
     # may crash if website returns error
     try:
-        page = urllib.request.urlopen(request)
+        page = urllib.request.urlopen(request, timeout=2)
     
         # lxml - html/ xml processor
         soup = BeautifulSoup(page, "lxml")
@@ -32,116 +34,135 @@ def word_count(url):
             words_count += len(info.get_text().split())
         
     except:
-        print("Error with webpage!")
+        print("Error with webpage!\n")
         return 0
     
     return words_count
 
-# regular google search results, but gives results such as youtube, etc
-def google_search():
+# parse a webpage, if suitable, save as html file
+def parse_webpage(target_function):
+    threads = []
+
     for term in search_terms:
-        results = 0
-        current_page = 1
+        thread = threading.Thread(target=target_function, args=(term,))
+        threads.append(thread)
+        thread.start()
 
-        # make directory for search results
-        directory = os.path.join(os.getcwd(), "Dataset", term.replace(" ", "_"))
-        os.mkdir(directory)
+    for thread in threads:
+        thread.join()
 
-        # keep going through google results until we have correct number of documents
-        while results < MAX_DOCUMENTS:
-            url = "https://www.google.com/search?q=" + term.replace(" ", "+") + page_delimiter.format(current_page * 10)
-            request = urllib.request.Request(url, headers=header)
-            page = urllib.request.urlopen(request)
-            # lxml - html/ xml processor
-            soup = BeautifulSoup(page, "lxml")
-            # finds all the headers from google search
-            for info in soup.find_all("h3"):
-                # check that header belongs to an anchor
-                if info.parent.name == "a":
-                    url = info.parent["href"]
-                    print("header " + info.text)
-                    print("url " + url)
-                    print()
+# regular google search results, but gives results such as youtube, etc
+def google_search(term):
+
+    results = 0
+    current_page = 1
+
+    # make directory for search results
+    directory = os.path.join(os.getcwd(), "Dataset", term.replace(" ", "_"))
+    os.mkdir(directory)
+
+    # keep going through google results until we have correct number of documents
+    while results < MAX_DOCUMENTS:
+        url = "https://www.google.com/search?q=" + term.replace(" ", "+") + page_delimiter.format(current_page * 10)
+        request = urllib.request.Request(url, headers=header)
+        page = urllib.request.urlopen(request)
+        # lxml - html/ xml processor
+        soup = BeautifulSoup(page, "lxml")
+        # finds all the headers from google search
+        for info in soup.find_all("h3"):
+            # check that header belongs to an anchor
+            if info.parent.name == "a":
+                url = info.parent["href"]
+
+                # check word count, if under, skip website
+                words = word_count(url)
+
+                if (words < WORDS_MINIMUM):
+                    print("header " + info.text + "\nurl " + url + "\nword count < " + str(WORDS_MINIMUM) + " skipping..." + "\n")
+                    continue
+                else:
+                    print("header " + info.text + "\nurl " + url + "\nword count " + str(words) + "\n")
                     results += 1
-                    # use try/catch since some websites may 403
-                    try:
-                        # save to file
-                        search_result = urllib.request.urlopen(url)
-                        fp = open(os.path.join(directory, "articles{}.html".format(results)), "wb")
-                        fp.write(search_result.read())
-                        fp.close()
-                    except:
-                        print("error when writing file, skipping")
-                        print()
-                        results -= 1
-                    # check if we have enough results
-                    if results == MAX_DOCUMENTS:
-                        break
 
-            if results == MAX_DOCUMENTS:
-                break
-            current_page += 1
+                # use try/catch since some websites may 403
+                try:
+                    # save to file
+                    search_result = urllib.request.urlopen(url)
+                    fp = open(os.path.join(directory, "articles{}.html".format(results)), "wb")
+                    fp.write(search_result.read())
+                    fp.close()
+                except:
+                    print("error when writing file, skipping\n")
+                    results -= 1
+                # check if we have enough results
+                if results == MAX_DOCUMENTS:
+                    break
 
-def google_news_search():
+        if results == MAX_DOCUMENTS:
+            break
+        current_page += 1
+
+def google_news_search(term):
     
     attributes = {"role": "heading"}
 
-    for term in search_terms:
-        results = 0
-        current_page = 1
+    results = 0
+    current_page = 1
 
-        # make directory for search results
-        directory = os.path.join(os.getcwd(), "Dataset", term.replace(" ", "_"))
-        os.mkdir(directory)
-        
-        # keep going through google news results until we have correct number of documents
-        while results < MAX_DOCUMENTS:
-            url = "https://www.google.com/search?q=" + term.replace(" ", "+") + page_delimiter.format(current_page * 10) + "&tbm=nws"
-            request = urllib.request.Request(url, headers=header)
-            page = urllib.request.urlopen(request)
-            # lxml - html/ xml processor
-            soup = BeautifulSoup(page, "lxml")
-            # find the div with the news article
-            for info in soup.find_all("div", attributes):
-                # check that header belongs to an anchor
-                if info.parent.parent.parent.name == "a":
-                    url = info.parent.parent.parent["href"]
-                    print("header " + info.text)
-                    print("url " + url)
-                    # check word count, if under, skip website
-                    words = word_count(url)
-                    print("word count " + str(words))
-                    if (words < WORDS_MINIMUM):
-                        print("word count < " + str(WORDS_MINIMUM) + " skipping...")
-                        print()
-                        continue
-                    print()
+    # make directory for search results
+    directory = os.path.join(os.getcwd(), "Dataset", term.replace(" ", "_"))
+    os.mkdir(directory)
+    
+    # keep going through google news results until we have correct number of documents
+    while results < MAX_DOCUMENTS:
+        url = "https://www.google.com/search?q=" + term.replace(" ", "+") + page_delimiter.format(current_page * 10) + "&tbm=nws"
+        request = urllib.request.Request(url, headers=header)
+        page = urllib.request.urlopen(request)
+        # lxml - html/ xml processor
+        soup = BeautifulSoup(page, "lxml")
+        # find the div with the news article
+        for info in soup.find_all("div", attributes):
+            # check that header belongs to an anchor
+            if info.parent.parent.parent.name == "a":
+                url = info.parent.parent.parent["href"]
+                
+                # check word count, if under, skip website
+                words = word_count(url)
+
+                if (words < WORDS_MINIMUM):
+                    print("header " + info.text + "\nurl " + url + "\nword count < " + str(WORDS_MINIMUM) + " skipping..." + "\n")
+                    continue
+                else:
+                    print("header " + info.text + "\nurl " + url + "\nword count " + str(words) + "\n")
                     results += 1
-                    # use try/catch since some websites may 403
-                    try:
-                        # save to file
-                        search_result = urllib.request.urlopen(url)
-                        fp = open(os.path.join(directory, "articles{}.html".format(results)), "wb")
-                        fp.write(search_result.read())
-                        fp.close()
-                    except:
-                        print("error when writing file, skipping")
-                        print()
-                        results -= 1
-                    # check if we have enough results
-                    if results == MAX_DOCUMENTS:
-                        break
+                # use try/catch since some websites may 403
+                try:
+                    # save to file
+                    search_result = urllib.request.urlopen(url)
+                    fp = open(os.path.join(directory, "articles{}.html".format(results)), "wb")
+                    fp.write(search_result.read())
+                    fp.close()
+                except:
+                    print("error when writing file, skipping\n")
+                    results -= 1
+                # check if we have enough results
+                if results == MAX_DOCUMENTS:
+                    break
 
-            if results == MAX_DOCUMENTS:
-                break
-            current_page += 1
+        if results == MAX_DOCUMENTS:
+            break
+        current_page += 1
 
 def main():
-    # timer
+    # start time
     start = time.time()
-    # google_search()
-    google_news_search()
+
+    parse_webpage(google_news_search)
+
+    # end time
     end = time.time()
+
     print("Elapsed time: {:.2f} seconds".format(end - start))
 
-main()
+if __name__ == "__main__":
+    main()
